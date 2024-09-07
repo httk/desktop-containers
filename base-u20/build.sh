c7d00b780b0a356248b99d81515acea8d8a723d8
Rickard Armiento <rickard-gpg@armiento.net>
2024-09-07 11:49:17 +0200

    Fix ubuntu 20.04 linewrap

--- base-u20/build.sh
+++ base-u20/build.sh
@@ -12,12 +12,9 @@ FROM $BASE
 ENV DEBIAN_FRONTEND noninteractive
 ENV TZ Europe/Stockholm
 RUN apt-get update && apt-get -y dist-upgrade && apt-get install -y --reinstall ca-certificates 
-RUN apt-get -y --no-install-recommends install locales nano wget bash curl git jq less net-tools p7zip-full patch pciutils pkg-config procps psmisc
- psutils rsync screen unzip xmlstarlet xz-utils python3 python3-pip python3-setuptools python3-venv libx11-data gpg gpg-agent xdg-utils iproute2 al
-sa-utils && apt-get clean autoclean -y && apt-get autoremove -y && rm -rf /var/tmp/* && rm -rf /tmp/*
+RUN apt-get -y --no-install-recommends install locales nano wget bash curl git jq less net-tools p7zip-full patch pciutils pkg-config procps psmisc psutils rsync screen unzip xmlstarlet xz-utils python3 python3-pip python3-setuptools python3-venv libx11-data gpg gpg-agent xdg-utils iproute2 alsa-utils && apt-get clean autoclean -y && apt-get autoremove -y && rm -rf /var/tmp/* && rm -rf /tmp/*
 COPY en_SE.locale /tmp/en_SE.locale
-RUN test ! -e /usr/share/i18n/locales/en_SE && cp /tmp/en_SE.locale /usr/share/i18n/locales/en_SE && localedef -i en_SE -f UTF-8 en_SE.UTF-8 && ech
-o "# en_SE.UTF-8 UTF-8" >> "/etc/locale.gen" && locale-gen ${LOCALES} && update-locale "LANG=$LANG"
+RUN test ! -e /usr/share/i18n/locales/en_SE && cp /tmp/en_SE.locale /usr/share/i18n/locales/en_SE && localedef -i en_SE -f UTF-8 en_SE.UTF-8 && echo "# en_SE.UTF-8 UTF-8" >> "/etc/locale.gen" && locale-gen ${LOCALES} && update-locale "LANG=$LANG"
 ENV LANG $LANG
 RUN groupadd -r -g 5000 build && useradd -m -u 5000 -g 5000 -c "Build user" "build"
 RUN groupadd -r -g "$UID" "$USER" && useradd -m -u "$UID" -g "$UID" -c "$FULLNAME" "$USER" && mkdir /tmp/$USER && chown "$USER:$USER" "/tmp/$USER" 
