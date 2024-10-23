e149f257225ced3bc84d692d34ea4171cdd9a593
Rickard Armiento <rickard-gpg@armiento.net>
2024-10-23 13:17:41 +0200

    Merge changes building fat-u24

--- images/fat-u24/build.sh
+++ images/fat-u24/build.sh
@@ -14,6 +14,7 @@ LOCALES="$(locale -a | grep -v "POSIX" | sed 's/.utf8/.UTF-8/' | tr '\n' ' ')"
 
 cat > ./Containerfile <<EOF
 FROM $BASE
+ENV DEBIAN_FRONTEND=noninteractive
 RUN dpkg --add-architecture i386 && apt-get update && apt-get -y dist-upgrade && apt-get install -y --reinstall ca-certificates
 RUN apt-get -y --no-install-recommends install locales nano wget bash curl git jq less net-tools p7zip-full patch pciutils pkg-config procps psmisc psutils rsync screen unzip xmlstarlet xz-utils python3 python3-pip python3-setuptools python3-venv libx11-data x11-utils gpg gpg-agent xdg-utils iproute2 pipewire pipewire-alsa pipewire-pulse alsa-utils libasound2-plugins foot weston xterm mesa-utils xwayland openbox dbus dbus-x11 bubblewrap steam-installer libsdl2-2.0-0 libseat1 libxres1 libinput10 falkon libpipewire-0.3-0 libpipewire-0.3-common pipewire-bin xdg-desktop-portal xdg-desktop-portal-gnome xdg-desktop-portal-gtk && apt-get clean autoclean -y && apt-get autoremove -y && rm -rf /var/tmp/* && rm -rf /tmp/*
 # RUN apt-get -y install gamescope ## Not working for 24.04 :-(
@@ -27,7 +28,8 @@ COPY ./files/gamescope-on-vc /usr/local/bin/gamescope-on-vc
 RUN chmod +x /usr/bin/xwayland-exec /usr/bin/xwayland-wm-exec
 RUN mkdir -p /tmp/.X11-unix && chmod a+rwxt /tmp/.X11-unix
 RUN ln -s /opt/.X11-unix/X42 /tmp/.X11-unix/X42
-RUN test ! -e /usr/share/i18n/locales/en_SE && cp /tmp/en_SE.locale /usr/share/i18n/locales/en_SE && localedef -i en_SE -f UTF-8 en_SE.UTF-8 && update-locale "LANG=$LANG"
+RUN test ! -e /usr/share/i18n/locales/en_SE && cp /tmp/en_SE.locale /usr/share/i18n/locales/en_SE && localedef -i en_SE -f UTF-8 en_SE.UTF-8
+RUN locale-gen && update-locale "LANG=$LANG"
 ENV LANG $LANG
 RUN groupadd -r -g 5000 build && useradd -m -u 5000 -g 5000 -c "Build user" "build" && ln -s "/tmp/$USER/run" "$XDG_RUNTIME_DIR"
 EOF
